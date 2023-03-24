47168cc8ab05dd84cfb20b67ea024db1336e2121
Lyubsii <gordyuhina_lyuba@mail.ru>
2023-03-24 16:33:24 +0300

    3 commit

--- src/test/java/PostmanEcho3Test.java
+++ src/test/java/PostmanEcho3Test.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEcho3Test {
@@ -20,7 +19,6 @@ public class PostmanEcho3Test {
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("some data"))
-                .body(matchesJsonSchemaInClasspath("postmanEcho.schema.json"))
         ;
     }
 
